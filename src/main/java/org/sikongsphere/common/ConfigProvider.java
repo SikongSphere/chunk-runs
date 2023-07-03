31e2cde7fca07b2f10fa3e6c4b9e18573c85b5a4
achaochao <liangc0427@gmail.com>
2023-07-03 23:29:52 +0800

    update

---
+++ src/main/java/org/sikongsphere/common/ConfigProvider.java
@@ -0,0 +1,10 @@
+package org.sikongsphere.common;
+
+public class ConfigProvider {
+
+    public static final String HBASE_CATALOG = "hbase.catalog";
+
+    public static final String HBASE_ZK = "hbase.zookeepers";
+    public static final String ZK_HOST = "sikongsphere-chunkruns:2181";
+    public static final String USER_NAME = "root";
+}
