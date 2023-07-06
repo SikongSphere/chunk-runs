bb6070508651dbac7b4b1dfba2362c02813e7985
a-chaochao <liangc0427@gmail.com>
2023-07-06 16:07:31 +0800

    update

--- src/main/java/org/sikongsphere/geomesa/SimpleFeatureUtils.java
+++ src/main/java/org/sikongsphere/geomesa/SimpleFeatureUtils.java
@@ -8,10 +8,11 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
 */
-package org.sikongsphere.common;
+package org.sikongsphere.geomesa;
 
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
+import org.sikongsphere.common.ConfigProvider;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -19,28 +20,17 @@ import java.util.Map;
 
 /**
  * @author Liang Chao
- * @date 2023/7/5 11:46
+ * @date 2023/7/6 15:50
  */
-
-public class ConfigProvider {
-
-    /**
-     * strings constants.
-     */
-    public static final String HBASE_CATALOG = "hbase.catalog";
-    public static final String HBASE_ZK = "hbase.zookeepers";
-    public static final String ZK_HOST = "geomesa-hbase-local:2181";
-    public static final String TABLE_NAME = "chunkruns_table";
+public class SimpleFeatureUtils {
 
     /**
-     * used to make connection with geomesa-hbase.
-     * @param tableName name of your table.
-     * @return
-     * @throws IOException
+     *此方法用于连接HBase
+     * @param tableName 用于创建主表的名称
+     * @return Datastore 用于写入数据的容器
      */
-    public static DataStore mkConnection(String tableName) throws IOException {
+    public DataStore mkConnection(String tableName) throws IOException {
         Map<String, String> params = new HashMap<String, String>();
-
         params.put(ConfigProvider.HBASE_CATALOG, tableName);
         params.put(ConfigProvider.HBASE_ZK, ConfigProvider.ZK_HOST);
 
